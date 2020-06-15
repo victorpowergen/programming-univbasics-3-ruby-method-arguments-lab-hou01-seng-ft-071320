dcb21d404d7cacd6ff64ec9a50733f4f93b677b0
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-15 09:42:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,12 @@ def introduction(name)
 puts "Hi, my name is #{name}."
 
 end
-introduction(Roy)
+introduction
 
+def introduction_with_language (name, language)
+puts "Hi, my name is #{name} && I am learning to program in #{language}."
 
+introduction_with_language_optional (name, language "Ruby")
 
 
 
